e4b0028e029d644c864ba761f53032cd06efad76
Jeremy W <jtwillo51@gmail.com>
2020-08-05 18:33:43 -0600

    done for tonight

---
+++ src/codeChallenge.py
@@ -0,0 +1,9 @@
+random = ["Joe", "2", "Ted", "4.98", "14", "Sam", "void *", "42", "float", "pointers", "5006"]
+
+i = 0
+# while i < len(random):
+#     print(random[i])
+#     i += 1
+
+for i in random:
+    print(i)
